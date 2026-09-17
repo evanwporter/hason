6ee7cdbc1f6eb214977ab77814d129e156310513
Evan Porter <evanwporter@gmail.com>
2026-09-16 22:08:21 -0700

    setup flake dev env

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+	description = "Haskell development shell";
+
+	inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+	outputs = {
+		self,
+		nixpkgs,
+	}: let
+		systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+		forAllSystems = nixpkgs.lib.genAttrs systems;
+	in {
+		devShells =
+			forAllSystems (system: let
+					pkgs = import nixpkgs {inherit system;};
+				in {
+					default =
+						pkgs.mkShell {
+							packages = with pkgs; [
+								ghc
+								haskell-language-server
+								cabal-install
+								hlint
+								pkg-config
+								zlib
+							];
+
+							shellHook = ''
+								echo "Haskell shell: $(ghc --numeric-version)"
+							'';
+						};
+				});
+	};
+}
